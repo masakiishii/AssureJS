1e6450b9c5a07ffeeada9d64edc7a2845765af67
Okamoto Yuki <yuki@Okamotos-MacBook-Pro.local>
2013-08-06 18:01:41 +0900

    add CaseEncoder

---
+++ src/CaseEncoder.js
@@ -0,0 +1,12 @@
+var AssureIt;
+(function (AssureIt) {
+    var CaseEncoder = (function () {
+        function CaseEncoder() {
+        }
+        CaseEncoder.prototype.ToJson = function (root) {
+            return null;
+        };
+        return CaseEncoder;
+    })();
+    AssureIt.CaseEncoder = CaseEncoder;
+})(AssureIt || (AssureIt = {}));
