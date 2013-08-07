1ec180a9cf4f8c78da6c6c5014805e96a640f891
Yusuke-Nakayama-lab <yusuke.nakayama1218@gmail.com>
2013-08-07 17:19:38 +0900

    add two function to encode

--- src/CaseEncoder.js
+++ src/CaseEncoder.js
@@ -1,10 +1,51 @@
 var AssureIt;
 (function (AssureIt) {
+    var JsonNodeModel = (function () {
+        function JsonNodeModel() {
+        }
+        return JsonNodeModel;
+    })();
+    AssureIt.JsonNodeModel = JsonNodeModel;
+
     var CaseEncoder = (function () {
         function CaseEncoder() {
         }
-        CaseEncoder.prototype.ToJson = function (root) {
-            return null;
+        CaseEncoder.prototype.ConvertToJson = function (root) {
+            this.JsonRoot = new JsonNodeModel();
+            this.JsonRoot.Type = root.Type;
+            this.JsonRoot.Label = root.Label;
+            this.JsonRoot.Statement = root.Statement;
+            this.JsonRoot.Annotations = root.Annotations;
+            this.JsonRoot.Notes = root.Notes;
+
+            var JsonChildNodes = new Array();
+            for (var i = 0; i < root.Children.length; i++) {
+                JsonChildNodes[i] = new JsonNodeModel();
+                this.GetChild(root.Children[i], JsonChildNodes[i]);
+            }
+
+            this.JsonRoot.Children = JsonChildNodes;
+
+            console.log(this.JsonRoot);
+            return this.JsonRoot;
+        };
+
+        CaseEncoder.prototype.GetChild = function (root, JsonNode) {
+            JsonNode.Type = root.Type;
+            JsonNode.Label = root.Label;
+            JsonNode.Statement = root.Statement;
+            JsonNode.Annotations = root.Annotations;
+            JsonNode.Notes = root.Notes;
+
+            var ChildNodes = new Array();
+            for (var i = 0; i < root.Children.length; i++) {
+                ChildNodes[i] = new JsonNodeModel();
+                this.GetChild(root.Children[i], ChildNodes[i]);
+            }
+
+            JsonNode.Children = ChildNodes;
+
+            return JsonNode;
         };
         return CaseEncoder;
     })();
--- src/CaseEncoder.ts
+++ src/CaseEncoder.ts
@@ -2,15 +2,61 @@
 
 module AssureIt {
 
-	export class CaseEncoder {
+	export class JsonNodeModel {
+		Type: NodeType;
+		Label: string;
+		Statement: string;
+		Annotations: CaseAnnotation[];
+		Notes: CaseNote[];
+		Children: JsonNodeModel[];
 
 		constructor() {
 		}
 
-		ToJson(root: NodeModel /* add more args if you need */): any {
-			return null; // FIXME
-		}
-
 	}
 
+	export class CaseEncoder {
+		JsonRoot: JsonNodeModel;
+
+		constructor() {
+		}
+
+		ConvertToJson(root: NodeModel): JsonNodeModel {
+			this.JsonRoot = new JsonNodeModel();
+			this.JsonRoot.Type = root.Type;
+			this.JsonRoot.Label = root.Label;
+			this.JsonRoot.Statement = root.Statement;
+			this.JsonRoot.Annotations = root.Annotations;
+			this.JsonRoot.Notes = root.Notes;
+
+			var JsonChildNodes: JsonNodeModel[] = new Array<JsonNodeModel>();
+			for (var i: number = 0; i < root.Children.length; i++) {
+				JsonChildNodes[i] = new JsonNodeModel();
+				this.GetChild(root.Children[i], JsonChildNodes[i]);
+			}
+
+			this.JsonRoot.Children = JsonChildNodes;
+
+			console.log(this.JsonRoot);
+			return this.JsonRoot;
+		}
+
+		GetChild(root: NodeModel, JsonNode: JsonNodeModel): JsonNodeModel {
+			JsonNode.Type = root.Type;
+			JsonNode.Label = root.Label;
+			JsonNode.Statement = root.Statement;
+			JsonNode.Annotations = root.Annotations;
+			JsonNode.Notes = root.Notes;
+
+			var ChildNodes: JsonNodeModel[] = new Array<JsonNodeModel>();
+			for (var i: number = 0; i < root.Children.length; i++) {
+				ChildNodes[i] = new JsonNodeModel();
+				this.GetChild(root.Children[i], ChildNodes[i]);
+			}
+
+			JsonNode.Children = ChildNodes;
+
+			return JsonNode;
+		}
+	}
 }
