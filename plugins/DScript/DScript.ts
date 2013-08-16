bf78cfad52826befad86d0170c722ccdfe1b6145
Shunsuke Shida <shunsuke.sida@gmail.com>
2013-08-16 01:03:05 -0700

    added a new class: DScriptEditor

--- plugins/DScript/DScript.ts
+++ plugins/DScript/DScript.ts
@@ -6,12 +6,19 @@ class DScriptPlugIn extends AssureIt.PlugIn {
 
 	constructor(public plugInManager: AssureIt.PlugInManager) {
 		super(plugInManager);
-		this.MenuBarContentsPlugIn = new DScriptMenuPlugIn(plugInManager);
+		this.ActionPlugIn = new DScriptEditorPlugIn(plugInManager);
+		this.MenuBarContentsPlugIn = new DScriptMenuPlugIn(plugInManager, this.ActionPlugIn);
 	}
 
 }
 
 class DScriptMenuPlugIn extends AssureIt.MenuBarContentsPlugIn {
+	editorPlugIn: DScriptEditorPlugIn;
+	constructor(plugInManager: AssureIt.PlugInManager, editorPlugIn: DScriptEditorPlugIn) {
+		super(plugInManager);
+		this.editorPlugIn = editorPlugIn;
+	}
+
 	IsEnabled(caseViewer: AssureIt.CaseViewer, caseModel: AssureIt.NodeModel) : boolean {
 		return true;
 	}
@@ -19,6 +26,15 @@ class DScriptMenuPlugIn extends AssureIt.MenuBarContentsPlugIn {
 	Delegate(caseViewer: AssureIt.CaseViewer, caseModel: AssureIt.NodeModel, element: JQuery, serverApi: AssureIt.ServerAPI): boolean {
 		console.log("Hello DScript");
 		element.append('<a href="#" ><img id="dscript"  src="'+serverApi.basepath+'images/icon.png" title="DScript" alt="dscript" /></a>');
+
+		/*TODO add event handler */
 		return true;
+	}
+}
+
+class DScriptEditorPlugIn extends AssureIt.ActionPlugIn {
+	constructor(plugInManager: AssureIt.PlugInManager) {
+		super(plugInManager);
+		/* TODO init textarea */
 	}
 }
