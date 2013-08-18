629122379971d5dd37b214c78af2ecfdb9f625fb
Shunsuke Shida <shunsuke.sida@gmail.com>
2013-08-18 08:49:25 -0700

    added DScript Editor

--- plugins/DScript/DScript.js
+++ plugins/DScript/DScript.js
@@ -27,12 +27,40 @@ var DScriptMenuPlugIn = (function (_super) {
     };
 
     DScriptMenuPlugIn.prototype.Delegate = function (caseViewer, caseModel, element, serverApi) {
+        var _this = this;
         element.append('<a href="#" ><img id="dscript"  src="' + serverApi.basepath + 'images/icon.png" title="DScript" alt="dscript" /></a>');
 
+        $('#dscript').unbind('dblclick');
         $('#dscript').click(function (ev) {
             var Generator = new DScriptGenerator(caseModel);
             var script = Generator.codegen();
             console.log(script);
+
+            $('#dscript-editor-wrapper').css({ display: 'block' }).addClass("animated fadeInDown").focus().one("blur", { node: caseModel }, function (e, node) {
+                e.stopPropagation();
+
+                var $this = $(this);
+                $this.addClass("animated fadeOutUp");
+                window.setTimeout(function () {
+                    $this.removeClass();
+                    $this.css({ display: 'none' });
+                }, 1300);
+            }).on("keydown", function (e) {
+                if (e.keyCode == 27) {
+                    e.stopPropagation();
+                    $('#fullscreen-editor-wrapper').blur();
+                }
+            });
+            _this.editorPlugIn.editor_left.setValue(script);
+            _this.editorPlugIn.editor_left.refresh();
+            _this.editorPlugIn.editor_right.refresh();
+            $('#CodeMirror').focus();
+            $('#background').click(function () {
+                $('#dscript-editor-wrapper').blur();
+            });
+            window.setTimeout(function () {
+                $('#dscript-editor-wrapper').removeClass();
+            }, 1300);
         });
         return true;
     };
@@ -43,8 +71,48 @@ var DScriptEditorPlugIn = (function (_super) {
     __extends(DScriptEditorPlugIn, _super);
     function DScriptEditorPlugIn(plugInManager) {
         _super.call(this, plugInManager);
+        this.editor_left = CodeMirror.fromTextArea(document.getElementById('dscript-editor-left'), {
+            lineNumbers: true,
+            mode: "text/x-csrc",
+            readOnly: true,
+            lineWrapping: true
+        });
+        this.editor_right = CodeMirror.fromTextArea(document.getElementById('dscript-editor-right'), {
+            lineNumbers: true,
+            mode: "text/x-csrc",
+            readOnly: true,
+            lineWrapping: true
+        });
+        $('#dscript-editor-wrapper').css({
+            position: 'absolute',
+            top: '5%',
+            left: '5%',
+            height: '90%',
+            width: '90%',
+            display: 'none',
+            background: 'rgba(255, 255, 255, 0.85)'
+        });
 
-        this.editor = null;
+        $(this.editor_left.getWrapperElement()).css({
+            width: '100%',
+            height: '100%'
+        });
+        $(this.editor_right.getWrapperElement()).css({
+            width: '100%',
+            height: '100%'
+        });
+        $('#dscript-editor-left').parent().css({
+            width: '50%',
+            height: '100%',
+            float: 'left',
+            display: 'block'
+        });
+        $('#dscript-editor-right').parent().css({
+            width: '50%',
+            height: '100%',
+            float: 'right',
+            display: 'block'
+        });
     }
     return DScriptEditorPlugIn;
 })(AssureIt.ActionPlugIn);
--- plugins/DScript/DScript.ts
+++ plugins/DScript/DScript.ts
@@ -31,27 +31,96 @@ class DScriptMenuPlugIn extends AssureIt.MenuBarContentsPlugIn {
 		//console.log("Hello DScript");
 		element.append('<a href="#" ><img id="dscript"  src="'+serverApi.basepath+'images/icon.png" title="DScript" alt="dscript" /></a>');
 
-		/*TODO add event handler */
+		$('#dscript').unbind('dblclick');
 		$('#dscript').click((ev) => {
 				var Generator: DScriptGenerator = new DScriptGenerator(caseModel);
 				var script: string = Generator.codegen();
 				console.log(script);
+
+				$('#dscript-editor-wrapper')
+					.css({display: 'block'})
+					.addClass("animated fadeInDown")
+					.focus()
+					.one("blur", {node : caseModel}, function(e: JQueryEventObject, node: JQuery) {
+						e.stopPropagation();
+
+						var $this = $(this);
+						$this.addClass("animated fadeOutUp");
+						window.setTimeout(function() {
+							$this.removeClass();
+							$this.css({display: 'none'});
+						}, 1300);
+					})
+					.on("keydown", function(e: JQueryEventObject) {
+						if(e.keyCode == 27 /* ESC */){
+							e.stopPropagation();
+							$('#fullscreen-editor-wrapper').blur();
+						}
+					});
+				this.editorPlugIn.editor_left.setValue(script);
+				this.editorPlugIn.editor_left.refresh();
+				this.editorPlugIn.editor_right.refresh();
+				$('#CodeMirror').focus();
+				$('#background').click(function(){
+					$('#dscript-editor-wrapper').blur(); 
+				});
+				window.setTimeout(function() {
+					$('#dscript-editor-wrapper').removeClass();
+				}, 1300);
 		});
 		return true;
 	}
 }
 
 class DScriptEditorPlugIn extends AssureIt.ActionPlugIn {
-	editor: any;
+	editor_left:  any;
+	editor_right: any;
 	constructor(plugInManager: AssureIt.PlugInManager) {
 		super(plugInManager);
-		/* TODO init textarea */
-		this.editor = null;
-		//this.editor = CodeMirror.fromTextArea(document.getElementById('dscript-editor'), {
-		//	lineNumbers: true,
-		//	mode: "text/x-csrc",
-		//	readOnly: true,
-		//	lineWrapping: true,
-		//});
+		this.editor_left = CodeMirror.fromTextArea(document.getElementById('dscript-editor-left'), {
+			lineNumbers: true,
+			mode: "text/x-csrc",
+			readOnly: true,
+			lineWrapping: true,
+		});
+		this.editor_right = CodeMirror.fromTextArea(document.getElementById('dscript-editor-right'), {
+			lineNumbers: true,
+			mode: "text/x-csrc",
+			readOnly: true,
+			lineWrapping: true,
+		});
+		$('#dscript-editor-wrapper').css({
+			position: 'absolute',
+			top: '5%',
+			left: '5%',
+			height: '90%',
+			width: '90%',
+			display: 'none',
+			background: 'rgba(255, 255, 255, 0.85)',
+		});
+
+		/* FIXME Replace it with sophisticated style. */
+		$(this.editor_left.getWrapperElement()).css({
+			width: '100%',
+			height: '100%',
+		});
+		$(this.editor_right.getWrapperElement()).css({
+			width: '100%',
+			height: '100%',
+		});
+		$('#dscript-editor-left').parent()
+			.css({
+				width: '50%',
+				height: '100%',
+				float: 'left',
+				display: 'block',
+			});
+		$('#dscript-editor-right').parent()
+			.css({
+				width: '50%',
+				height: '100%',
+				float: 'right',
+				display: 'block',
+			});
 	}
 }
