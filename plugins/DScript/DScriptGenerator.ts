89e2c7a26db775b14a325e29728e3277aa670a66
Shunsuke Shida <shunsuke.sida@gmail.com>
2013-08-20 03:39:01 -0700

    fixed generator

--- plugins/DScript/DScriptGenerator.ts
+++ plugins/DScript/DScriptGenerator.ts
@@ -19,7 +19,7 @@ class DScriptGenerator {
 			if (note.Name == 'Monitor' || note.Name == 'Recovery' || note.Name == 'Condition') {
 				if (note.Body.Description) {
 					res += '// ' + note.Name + '\n';
-					res += "void " + this.getMethodName(model) + '_' + note.Name + ' {\n';
+					res += "void " + this.getMethodName(model) + '_' + note.Name + '() {\n';
 					res += '    ' + note.Body.Description.replace(/\n/g, '\n    ');
 					res += '\n}\n\n';
 				}
