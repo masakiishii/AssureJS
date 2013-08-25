ae6d719581986e7f98068fc81ecdfa3165e46109
uchida <uchida.atsushi.8@gmail.com>
2013-08-26 02:00:21 +0900

    add Export PlugIn

---
+++ plugins/Export/Export.js
@@ -0,0 +1,156 @@
+var __extends = this.__extends || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    __.prototype = b.prototype;
+    d.prototype = new __();
+};
+var ExportPlugIn = (function (_super) {
+    __extends(ExportPlugIn, _super);
+    function ExportPlugIn(plugInManager) {
+        _super.call(this, plugInManager);
+        var plugin = new ExportActionPlugIn(plugInManager);
+        this.ActionPlugIn = plugin;
+        this.MenuBarContentsPlugIn = new ExportMenuPlugIn(plugInManager, plugin);
+    }
+    return ExportPlugIn;
+})(AssureIt.PlugInSet);
+
+var ExportMenuPlugIn = (function (_super) {
+    __extends(ExportMenuPlugIn, _super);
+    function ExportMenuPlugIn(plugInManager, editorPlugIn) {
+        _super.call(this, plugInManager);
+        this.editorPlugIn = editorPlugIn;
+    }
+    ExportMenuPlugIn.prototype.IsEnabled = function (caseViewer, caseModel) {
+        return true;
+    };
+
+    ExportMenuPlugIn.prototype.Delegate = function (caseViewer, caseModel, element, serverApi) {
+        element.append('<a href="#" ><img id="export-xml" src="' + serverApi.basepath + 'images/icon.png" title="Export XML" alt="XML" /></a>');
+        element.append('<a href="#" ><img id="export-pdf" src="' + serverApi.basepath + 'images/icon.png" title="Export PDF" alt="XML" /></a>');
+        element.append('<a href="#" ><img id="export-png" src="' + serverApi.basepath + 'images/icon.png" title="Export PNG" alt="XML" /></a>');
+        $('#export-pdf').unbind('click');
+        $('#export-xml').unbind('click');
+        $('#export-png').unbind('click');
+        $('#export-pdf').click(this.editorPlugIn.ExportPdf);
+        $('#export-xml').click(this.editorPlugIn.ExportXml);
+        $('#export-png').click(this.editorPlugIn.ExportPng);
+        return true;
+    };
+    return ExportMenuPlugIn;
+})(AssureIt.MenuBarContentsPlugIn);
+
+var ExportActionPlugIn = (function (_super) {
+    __extends(ExportActionPlugIn, _super);
+    function ExportActionPlugIn(plugInManager) {
+        _super.call(this, plugInManager);
+    }
+    ExportActionPlugIn.prototype.IsEnabled = function (caseViewer, case0) {
+        return true;
+    };
+
+    ExportActionPlugIn.prototype.Delegate = function (caseViewer, case0, serverApi) {
+        var self = this;
+
+        var ShowExport = function (type) {
+            var ExportType = type;
+            return function (ev) {
+                ev.stopPropagation();
+                var svg = self.CreateSVGDocument(caseViewer, case0.ElementTop);
+                svg = svg.replace("</svg></svg>", "</svg>");
+                self.executePost(serverApi.basepath + 'export.' + type, { "type": type, "svg": svg });
+            };
+        };
+
+        this.ExportPdf = ShowExport("pdf");
+        this.ExportXml = ShowExport("xml");
+        this.ExportPng = ShowExport("png");
+
+        return true;
+    };
+
+    ExportActionPlugIn.prototype.executePost = function (action, data) {
+        var $body = $(document.body);
+        var $form = $("<form>").attr({
+            "action": action,
+            "method": "post",
+            "target": "_blank"
+        }).hide().appendTo($body);
+
+        if (data !== undefined) {
+            for (var paramName in data) {
+                $('<input type="hidden">').attr({
+                    'name': paramName,
+                    'value': data[paramName]
+                }).appendTo($form);
+            }
+        }
+        $form.submit();
+        $form.empty().remove();
+    };
+
+    ExportActionPlugIn.prototype.foreachLine = function (str, max, callback) {
+        if (!callback)
+            return;
+        var rest = str;
+        var maxLength = max || 20;
+        maxLength = maxLength < 1 ? 1 : maxLength;
+        var length = 0;
+        var i = 0;
+        for (var pos = 0; pos < rest.length; ++pos) {
+            var code = rest.charCodeAt(pos);
+            length += code < 128 ? 1 : 2;
+            if (length > maxLength || rest.charAt(pos) == "\n") {
+                callback(rest.substr(0, pos), i);
+                if (rest.charAt(pos) == "\n") {
+                    pos++;
+                }
+                rest = rest.substr(pos, rest.length - pos);
+                pos = -1;
+                length = 0;
+                i++;
+            }
+        }
+        callback(rest, i);
+    };
+
+    ExportActionPlugIn.prototype.CreateSVGDocument = function (caseViewer, Element) {
+        var _this = this;
+        var SVG_NS = "http://www.w3.org/2000/svg";
+        var $svg = $('<svg width="100%" height="100%" version="1.1" xmlns="' + SVG_NS + '">');
+        var viewMap = caseViewer.ViewMap;
+
+        var TopView = viewMap[Element.Label];
+        var $svg = $('<svg width="100%" height="100%" version="1.1" xmlns="' + SVG_NS + '">');
+        $svg.append($("svg defs").clone(false));
+        var $target = $(document.createElementNS(SVG_NS, "g")).appendTo($svg);
+        Element.traverse(function (i, node) {
+            var nodeView = viewMap[node.Label];
+            var svg = nodeView.SVGShape.GetSVG();
+            var div = nodeView.HTMLDoc.DocBase[0];
+            var connector = nodeView.SVGShape.GetSVGPath();
+
+            $target.append($(svg).clone(false));
+            $target.append($(connector).clone(false));
+
+            var $svgtext = $(document.createElementNS(SVG_NS, "text")).attr({ x: div.offsetLeft, y: div.offsetTop + 10 });
+
+            $(document.createElementNS(SVG_NS, "tspan")).text(node.Label).attr("font-weight", "bold").appendTo($svgtext);
+
+            _this.foreachLine(node.Statement, 1 + ~~(div.offsetWidth * 2 / 13), function (linetext) {
+                $(document.createElementNS(SVG_NS, "tspan")).text(linetext).attr({ x: div.offsetLeft, dy: 15, "font-size": "13px" }).appendTo($svgtext);
+            });
+
+            $target.append($svgtext);
+        });
+        var $dummydiv = $("<div>").append($svg);
+        var header = '<?xml version="1.0" standalone="no"?>\n' + '<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">\n';
+        var doc = header + $dummydiv.html();
+        $svg.empty().remove();
+        return doc;
+    };
+
+    ExportActionPlugIn.prototype.DeleteFromDOM = function () {
+    };
+    return ExportActionPlugIn;
+})(AssureIt.ActionPlugIn);
---
+++ plugins/Export/Export.ts
@@ -0,0 +1,162 @@
+/// <reference path="../../src/CaseModel.ts" />
+/// <reference path="../../src/ServerApi.ts" />
+/// <reference path="../../src/PlugInManager.ts" />
+/// <reference path="../Editor/Editor.ts" />
+
+class ExportPlugIn extends AssureIt.PlugInSet {
+
+	constructor(plugInManager: AssureIt.PlugInManager) {
+		super(plugInManager);
+		var plugin: ExportActionPlugIn = new ExportActionPlugIn(plugInManager);
+		this.ActionPlugIn = plugin;
+		this.MenuBarContentsPlugIn = new ExportMenuPlugIn(plugInManager, plugin);
+	}
+
+}
+
+class ExportMenuPlugIn extends AssureIt.MenuBarContentsPlugIn {
+	editorPlugIn: ExportActionPlugIn;
+	constructor(plugInManager: AssureIt.PlugInManager, editorPlugIn: ExportActionPlugIn) {
+		super(plugInManager);
+		this.editorPlugIn = editorPlugIn;
+	}
+
+	IsEnabled(caseViewer: AssureIt.CaseViewer, caseModel: AssureIt.NodeModel): boolean {
+		return true;
+	}
+
+	Delegate(caseViewer: AssureIt.CaseViewer, caseModel: AssureIt.NodeModel, element: JQuery, serverApi: AssureIt.ServerAPI): boolean {
+		element.append('<a href="#" ><img id="export-xml" src="' + serverApi.basepath + 'images/icon.png" title="Export XML" alt="XML" /></a>');
+		element.append('<a href="#" ><img id="export-pdf" src="' + serverApi.basepath + 'images/icon.png" title="Export PDF" alt="XML" /></a>');
+		element.append('<a href="#" ><img id="export-png" src="' + serverApi.basepath + 'images/icon.png" title="Export PNG" alt="XML" /></a>');
+		$('#export-pdf').unbind('click');
+		$('#export-xml').unbind('click');
+		$('#export-png').unbind('click');
+		$('#export-pdf').click(this.editorPlugIn.ExportPdf);
+		$('#export-xml').click(this.editorPlugIn.ExportXml);
+		$('#export-png').click(this.editorPlugIn.ExportPng);
+		return true;
+	}
+}
+
+class ExportActionPlugIn extends AssureIt.ActionPlugIn {
+	ExportPdf: (ev: Event) => void;
+	ExportXml: (ev: Event) => void;
+	ExportPng: (ev: Event) => void;
+
+	constructor(plugInManager: AssureIt.PlugInManager) {
+		super(plugInManager);
+	}
+
+	IsEnabled (caseViewer: AssureIt.CaseViewer, case0: AssureIt.Case) : boolean {
+		return true;
+	}
+
+	Delegate(caseViewer: AssureIt.CaseViewer, case0: AssureIt.Case, serverApi: AssureIt.ServerAPI)  : boolean {
+		var self = this;
+
+		var ShowExport = function(type: string) : (ev: Event) => void {
+			var ExportType = type;
+			return  function (ev: Event) {
+				ev.stopPropagation();
+				var svg = self.CreateSVGDocument(caseViewer, case0.ElementTop); //FIXME
+				svg = svg.replace("</svg></svg>", "</svg>"); // for IE10 Bug
+				self.executePost(serverApi.basepath + 'export.' + type, {"type" : type, "svg" : svg});
+			};
+		}
+
+		this.ExportPdf = ShowExport("pdf");
+		this.ExportXml = ShowExport("xml");
+		this.ExportPng = ShowExport("png");
+
+		return true;
+	}
+
+	executePost(action, data): void {
+		var $body = $(document.body);
+		var $form = $("<form>").attr({
+			"action" : action,
+			"method" : "post",
+			"target" : "_blank",
+		}).hide().appendTo($body);
+		
+		if (data !== undefined) {
+			for (var paramName in data) {
+				$('<input type="hidden">').attr({
+					'name' : paramName,
+					'value' : data[paramName],
+				}).appendTo($form);
+			}
+		}
+		$form.submit();
+		$form.empty().remove();
+	}
+
+	foreachLine(str: string, max: number, callback) : void{
+		if(!callback) return;
+		var rest: string = str;
+		var maxLength: number = max || 20;
+		maxLength = maxLength < 1 ? 1 : maxLength;
+		var length = 0;
+		var i = 0;
+		for(var pos = 0; pos < rest.length; ++pos) {
+			var code = rest.charCodeAt(pos);
+			length += code < 128 ? 1 : 2;
+			if(length > maxLength || rest.charAt(pos) == "\n"){
+				callback(rest.substr(0, pos), i);
+				if(rest.charAt(pos) == "\n"){
+					pos++;
+				}
+				rest = rest.substr(pos, rest.length - pos);
+				pos = -1;
+				length = 0;
+				i++;
+			}
+		}
+		callback(rest, i);
+	}
+
+	CreateSVGDocument(caseViewer: AssureIt.CaseViewer, Element: AssureIt.NodeModel):any {
+		var SVG_NS = "http://www.w3.org/2000/svg";
+		var $svg = $('<svg width="100%" height="100%" version="1.1" xmlns="'+SVG_NS+'">');
+		var viewMap = caseViewer.ViewMap;
+
+		var TopView = viewMap[Element.Label];
+		var $svg = $('<svg width="100%" height="100%" version="1.1" xmlns="'+SVG_NS+'">');
+		$svg.append($("svg defs").clone(false));
+		var $target = $(document.createElementNS(SVG_NS, "g")).appendTo($svg);
+		Element.traverse((i, node) => {
+			var nodeView = viewMap[node.Label];
+			var svg  = nodeView.SVGShape.GetSVG();
+			var div  = nodeView.HTMLDoc.DocBase[0];
+			var connector = nodeView.SVGShape.GetSVGPath();
+
+			$target.append($(svg).clone(false));
+			$target.append($(connector).clone(false));
+
+			var $svgtext = $(document.createElementNS(SVG_NS, "text"))
+				.attr({x : div.offsetLeft, y : div.offsetTop + 10});
+
+			$(document.createElementNS(SVG_NS, "tspan"))
+				.text(node.Label).attr("font-weight", "bold").appendTo($svgtext);
+
+			this.foreachLine(node.Statement, 1+~~(div.offsetWidth * 2 / 13), (linetext) => {
+				$(document.createElementNS(SVG_NS, "tspan"))
+					.text(linetext)
+					.attr({x : div.offsetLeft, dy : 15, "font-size" : "13px"})
+					.appendTo($svgtext);
+			});
+
+			$target.append($svgtext);
+		});
+		var $dummydiv = $("<div>").append($svg);
+		var header = '<?xml version="1.0" standalone="no"?>\n' + 
+			'<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">\n';
+		var doc = header + $dummydiv.html();
+		$svg.empty().remove();
+		return doc;
+	}
+
+	DeleteFromDOM(): void {
+	}
+}
