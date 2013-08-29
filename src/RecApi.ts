94d87ecd0965d8d36392513fdb26a27abe5a6cf0
Okamoto Yuki <yuki@Okamotos-MacBook-Pro.local>
2013-08-29 18:58:34 +0900

    add necessary file

---
+++ src/RecApi.ts
@@ -0,0 +1,112 @@
+///<reference path='../d.ts/jquery.d.ts'/>
+
+module AssureIt {
+
+	function RemoteProcedureCall(uri: string, method: string, params: any): any {
+
+		var defaultSuccessCallback = function(res) {
+			// do nothing
+		}
+
+		var defaultErrorCallback = function(req, stat, err) {
+			alert("ajax error");
+		}
+
+		var cmd = {
+			jsonrpc: "2.0",
+			method: method,
+			id: 1,
+			params: params
+		};
+
+		var ret = JSON.parse($.ajax({
+			type: "POST",
+			url: uri,
+			async: false,
+			data: cmd,
+			//dataType: "json",   // FIXME
+			//contentType: "application/json; charset=utf-8",   // FIXME
+			success: defaultSuccessCallback,
+			error: defaultErrorCallback
+		}).responseText);
+
+		return ret;
+	}
+
+	export class RECAPI {
+		uri : string;
+		basepath : string;
+
+		constructor(basepath: string) {
+			this.uri = basepath + "/api/2.0/";
+			this.basepath = basepath;
+		}
+
+		getRawData(recid: string): any {
+			var params = { recid: recid };
+
+			var res = RemoteProcedureCall(this.uri, "getRawData", params);
+
+			if('result' in res) {
+				return res.result;
+			}
+			else {
+				console.log(res.error)
+				return null;
+			}
+		}
+
+		getLatestData(location: string, type: string): any {
+			var params = {
+				location: location,
+				type: type
+			};
+
+			var res = RemoteProcedureCall(this.uri, "getLatestData", params);
+
+			if('result' in res) {
+				return res.result;
+			}
+			else {
+				console.log(res.error)
+				return null;
+			}
+		}
+
+		getRawDataList(location: string, type: string, limit?: number, beginTimestamp?: Date, endTimestamp?: Date): any {
+			var params = {
+				location: location,
+				type: type,
+				limit: limit,
+				beginTimestamp: beginTimestamp,
+				endTimestamp: endTimestamp,
+			};
+
+			var res = RemoteProcedureCall(this.uri, "getRawtDataList", params);
+
+			if('result' in res) {
+				return res.result;
+			}
+			else {
+				console.log(res.error)
+				return null;
+			}
+		}
+
+		getMonitorList(): any[] {
+			var params = {};
+
+			var res = RemoteProcedureCall(this.uri, "getMonitorList", params);
+
+			if('result' in res) {
+				return res.result;
+			}
+			else {
+				console.log(res.error)
+				return null;
+			}
+		}
+
+	}
+
+}
