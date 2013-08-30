4d0f17fc198a9059531fe60afa5b90959a0c5615
Shunsuke Shida <shunsuke.sida@gmail.com>
2013-08-30 01:57:11 -0700

    updated mock: JSON to AssureNotation

--- mock/Index.ts
+++ mock/Index.ts
@@ -44,269 +44,272 @@ $(function () {
 		"DCaseName": "test",
 		"NodeCount": 25,
 		"TopGoalLabel": "G1",
-		"NodeList": [
-			{
-				"Children": [
-					"S1",
-					"C1"
-				],
-				"Statement": "",
-				"NodeType": 0,
-				"Label": "G1",
-				"Annotations": [],
-				"Notes": []
-			},
-			{
-				"Children": [
-				],
-				"Statement": "",
-				"NodeType": 1,
-				"Label": "C1",
-				"Annotations": [{"Name" : "Def", "Body" : "(a = 1)"}],
-				"Notes": []
-			},
-			{
-				"Children": [
-					"G2",
-					"G3"
-				],
-				"Statement": "",
-				"NodeType": 2,
-				"Label": "S1",
-				"Annotations": [],
-				"Notes": []
-			},
-			{
-				"Children": [
-					"S2"
-				],
-				"Statement": "",
-				"NodeType": 0,
-				"Label": "G2",
-				"Annotations": [],
-				"Notes": []
-			},
-			{
-				"Children": [
-					"G4",
-					"G5",
-					"G6",
-					"C3"
-				],
-				"Statement": "",
-				"NodeType": 2,
-				"Label": "S2",
-				"Annotations": [],
-				"Notes": []
-			},
-			{
-				"Children": [
-					"E1"
-				],
-				"Statement": "Hoge",
-				"NodeType": 0,
-				"Label": "G4",
-				"Annotations": [],
-				"Notes": []
-			},
-			{
-				"Children": [
-					"C2",
-					"E2",
-					"E3"
-				],
-				"Statement": "",
-				"NodeType": 0,
-				"Label": "G5",
-				"Annotations": [],
-				"Notes": []
-			},
-			{
-				"Children": [
-					"E4"
-				],
-				"Statement": "",
-				"NodeType": 0,
-				"Label": "G6",
-				"Annotations": [],
-				"Notes": []
-			},
-			{
-				"Children": [
-				],
-				"Statement": "",
-				"NodeType": 3,
-				"Label": "E1",
-				"Annotations": [],
-				"Notes": [
-				{
-					"Name": "Monitor",
-					"Body": {
-						"Description": "rec(1)"
-					}
-				}
-					]
-			},
-			{
-				"Children": [
-				],
-				"Statement": "",
-				"NodeType": 1,
-				"Label": "C2",
-				"Annotations": [{"Name" : "Def", "Body" : "(a = 2)"}],
-				"Notes": []
-			},
-			{
-				"Children": [
-					"C4"
-				],
-				"Statement": "",
-				"NodeType": 3,
-				"Label": "E2",
-				"Annotations": [],
-				"Notes": []
-			},
-			{
-				"Children": [
-				],
-				"Statement": "",
-				"NodeType": 1,
-				"Label": "C4",
-				"Annotations": [{"Name" : "Def", "Body" : "(a = 4)"}],
-				"Notes": []
-			},
-			{
-				"Children": [
-				],
-				"Statement": "",
-				"NodeType": 1,
-				"Label": "C3",
-				"Annotations": [{"Name" : "Def", "Body" : "(a = 3)"}],
-				"Notes": []
-			},
-			{
-				"Children": [
-				],
-				"Statement": "",
-				"NodeType": 3,
-				"Label": "E3",
-				"Annotations": [],
-				"Notes": []
-			},
-			{
-				"Children": [
-				],
-				"Statement": "",
-				"NodeType": 3,
-				"Label": "E4",
-				"Annotations": [],
-				"Notes": []
-			},
-			{
-				"Children": [
-					"S3"
-				],
-				"Statement": "",
-				"NodeType": 0,
-				"Label": "G3",
-				"Annotations": [],
-				"Notes": []
-			},
-			{
-				"Children": [
-					"G7",
-					"G8",
-					"G9"
-				],
-				"Statement": "",
-				"NodeType": 2,
-				"Label": "S3",
-				"Annotations": [],
-				"Notes": []
-			},
-			{
-				"Children": [
-					"C5",
-					"E5",
-					"E6",
-				],
-				"Statement": "",
-				"NodeType": 0,
-				"Label": "G7",
-				"Annotations": [],
-				"Notes": []
-			},
-			{
-				"Children": [
-				],
-				"Statement": "",
-				"NodeType": 3,
-				"Label": "E5",
-				"Annotations": [],
-				"Notes": []
-			},
-			{
-				"Children": [
-				],
-				"Statement": "",
-				"NodeType": 3,
-				"Label": "E6",
-				"Annotations": [],
-				"Notes": []
-			},
-			{
-				"Children": [
-				],
-				"Statement": "",
-				"NodeType": 1,
-				"Label": "C5",
-				"Annotations": [],
-				"Notes": []
-			},
-			{
-				"Children": [
-					"C6",
-					"E7"
-				],
-				"Statement": "",
-				"NodeType": 0,
-				"Label": "G8",
-				"Annotations": [],
-				"Notes": []
-			},
-			{
-				"Children": [
-				],
-				"Statement": "",
-				"NodeType": 0,
-				"Label": "G9",
-				"Annotations": [],
-				"Notes": []
-			},
-			{
-				"Children": [
-				],
-				"Statement": "",
-				"NodeType": 1,
-				"Label": "C6",
-				"Annotations": [],
-				"Notes": []
-			},
-			{
-				"Children": [
-				],
-				"Statement": "",
-				"NodeType": 3,
-				"Label": "E7",
-				"Annotations": [],
-				"Notes": []
-			},
-		]
+		"NodeList": "*G1\n*C1 @Def\n*S1\n**G2\n**S2\n**C3 @Def\n***G4\nHoge\n***E1\nMonitor::\n***G5\n***C2 @Def\n***E2\n***C4 @Def\n***E3\n***G6\n***E4\n**G3\n**S3\n***G7\n***C5\n***E5\n***E6\n***G8\n***C6\n***E7\n***G9",
+		//"NodeList": "*G1\n*S1\n*C",
+		//"NodeList": [
+		//	{
+		//		"Children": [
+		//			"S1",
+		//			"C1"
+		//		],
+		//		"Statement": "",
+		//		"NodeType": 0,
+		//		"Label": "G1",
+		//		"Annotations": [],
+		//		"Notes": []
+		//	},
+		//	{
+		//		"Children": [
+		//		],
+		//		"Statement": "",
+		//		"NodeType": 1,
+		//		"Label": "C1",
+		//		"Annotations": [{"Name" : "Def", "Body" : "(a = 1)"}],
+		//		"Notes": []
+		//	},
+		//	{
+		//		"Children": [
+		//			"G2",
+		//			"G3"
+		//		],
+		//		"Statement": "",
+		//		"NodeType": 2,
+		//		"Label": "S1",
+		//		"Annotations": [],
+		//		"Notes": []
+		//	},
+		//	{
+		//		"Children": [
+		//			"S2"
+		//		],
+		//		"Statement": "",
+		//		"NodeType": 0,
+		//		"Label": "G2",
+		//		"Annotations": [],
+		//		"Notes": []
+		//	},
+		//	{
+		//		"Children": [
+		//			"G4",
+		//			"G5",
+		//			"G6",
+		//			"C3"
+		//		],
+		//		"Statement": "",
+		//		"NodeType": 2,
+		//		"Label": "S2",
+		//		"Annotations": [],
+		//		"Notes": []
+		//	},
+		//	{
+		//		"Children": [
+		//			"E1"
+		//		],
+		//		"Statement": "Hoge",
+		//		"NodeType": 0,
+		//		"Label": "G4",
+		//		"Annotations": [],
+		//		"Notes": []
+		//	},
+		//	{
+		//		"Children": [
+		//			"C2",
+		//			"E2",
+		//			"E3"
+		//		],
+		//		"Statement": "",
+		//		"NodeType": 0,
+		//		"Label": "G5",
+		//		"Annotations": [],
+		//		"Notes": []
+		//	},
+		//	{
+		//		"Children": [
+		//			"E4"
+		//		],
+		//		"Statement": "",
+		//		"NodeType": 0,
+		//		"Label": "G6",
+		//		"Annotations": [],
+		//		"Notes": []
+		//	},
+		//	{
+		//		"Children": [
+		//		],
+		//		"Statement": "",
+		//		"NodeType": 3,
+		//		"Label": "E1",
+		//		"Annotations": [],
+		//		"Notes": [
+		//		{
+		//			"Name": "Monitor",
+		//			"Body": {
+		//				"Description": "rec(1)"
+		//			}
+		//		}
+		//			]
+		//	},
+		//	{
+		//		"Children": [
+		//		],
+		//		"Statement": "",
+		//		"NodeType": 1,
+		//		"Label": "C2",
+		//		"Annotations": [{"Name" : "Def", "Body" : "(a = 2)"}],
+		//		"Notes": []
+		//	},
+		//	{
+		//		"Children": [
+		//			"C4"
+		//		],
+		//		"Statement": "",
+		//		"NodeType": 3,
+		//		"Label": "E2",
+		//		"Annotations": [],
+		//		"Notes": []
+		//	},
+		//	{
+		//		"Children": [
+		//		],
+		//		"Statement": "",
+		//		"NodeType": 1,
+		//		"Label": "C4",
+		//		"Annotations": [{"Name" : "Def", "Body" : "(a = 4)"}],
+		//		"Notes": []
+		//	},
+		//	{
+		//		"Children": [
+		//		],
+		//		"Statement": "",
+		//		"NodeType": 1,
+		//		"Label": "C3",
+		//		"Annotations": [{"Name" : "Def", "Body" : "(a = 3)"}],
+		//		"Notes": []
+		//	},
+		//	{
+		//		"Children": [
+		//		],
+		//		"Statement": "",
+		//		"NodeType": 3,
+		//		"Label": "E3",
+		//		"Annotations": [],
+		//		"Notes": []
+		//	},
+		//	{
+		//		"Children": [
+		//		],
+		//		"Statement": "",
+		//		"NodeType": 3,
+		//		"Label": "E4",
+		//		"Annotations": [],
+		//		"Notes": []
+		//	},
+		//	{
+		//		"Children": [
+		//			"S3"
+		//		],
+		//		"Statement": "",
+		//		"NodeType": 0,
+		//		"Label": "G3",
+		//		"Annotations": [],
+		//		"Notes": []
+		//	},
+		//	{
+		//		"Children": [
+		//			"G7",
+		//			"G8",
+		//			"G9"
+		//		],
+		//		"Statement": "",
+		//		"NodeType": 2,
+		//		"Label": "S3",
+		//		"Annotations": [],
+		//		"Notes": []
+		//	},
+		//	{
+		//		"Children": [
+		//			"C5",
+		//			"E5",
+		//			"E6",
+		//		],
+		//		"Statement": "",
+		//		"NodeType": 0,
+		//		"Label": "G7",
+		//		"Annotations": [],
+		//		"Notes": []
+		//	},
+		//	{
+		//		"Children": [
+		//		],
+		//		"Statement": "",
+		//		"NodeType": 3,
+		//		"Label": "E5",
+		//		"Annotations": [],
+		//		"Notes": []
+		//	},
+		//	{
+		//		"Children": [
+		//		],
+		//		"Statement": "",
+		//		"NodeType": 3,
+		//		"Label": "E6",
+		//		"Annotations": [],
+		//		"Notes": []
+		//	},
+		//	{
+		//		"Children": [
+		//		],
+		//		"Statement": "",
+		//		"NodeType": 1,
+		//		"Label": "C5",
+		//		"Annotations": [],
+		//		"Notes": []
+		//	},
+		//	{
+		//		"Children": [
+		//			"C6",
+		//			"E7"
+		//		],
+		//		"Statement": "",
+		//		"NodeType": 0,
+		//		"Label": "G8",
+		//		"Annotations": [],
+		//		"Notes": []
+		//	},
+		//	{
+		//		"Children": [
+		//		],
+		//		"Statement": "",
+		//		"NodeType": 0,
+		//		"Label": "G9",
+		//		"Annotations": [],
+		//		"Notes": []
+		//	},
+		//	{
+		//		"Children": [
+		//		],
+		//		"Statement": "",
+		//		"NodeType": 1,
+		//		"Label": "C6",
+		//		"Annotations": [],
+		//		"Notes": []
+		//	},
+		//	{
+		//		"Children": [
+		//		],
+		//		"Statement": "",
+		//		"NodeType": 3,
+		//		"Label": "E7",
+		//		"Annotations": [],
+		//		"Notes": []
+		//	},
+		//]
 	}
 
 	var Case0: AssureIt.Case = new AssureIt.Case(JsonData.DCaseName, 1, 0, pluginManager);
 	var caseDecoder: AssureIt.CaseDecoder = new AssureIt.CaseDecoder();
-	var root: AssureIt.NodeModel = caseDecoder.ParseJson(Case0, JsonData);
+	var root: AssureIt.NodeModel = caseDecoder.ParseASN(Case0, JsonData.NodeList, null);
+	console.log(root);
 
 	Case0.SetElementTop(root);
 
