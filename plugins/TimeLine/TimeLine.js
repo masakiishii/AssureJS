2ea349128f5329c90fb3c97c3fa09a5e00449961
uchida <uchida.atsushi.8@gmail.com>
2013-09-13 21:55:27 +0900

    remove unused code

--- plugins/TimeLine/TimeLine.js
+++ plugins/TimeLine/TimeLine.js
@@ -4,51 +4,6 @@ var __extends = this.__extends || function (d, b) {
     __.prototype = b.prototype;
     d.prototype = new __();
 };
-var TimeLine = (function () {
-    function TimeLine(caseViewer, nodeModel, element, serverApi) {
-        this.caseViewer = caseViewer;
-        this.nodeModel = nodeModel;
-        this.element = element;
-        this.serverApi = serverApi;
-    }
-    TimeLine.prototype.CreateDOM = function () {
-        this.root = $(this.caseViewer.Screen.ControlLayer);
-
-        var node = $("#" + this.nodeModel.Label);
-
-        this.container = $("<div></div>").css({
-            position: "absolute",
-            left: node.position().left + (node.width() / 2),
-            top: node.position().top + node.height() + 53
-        }).addClass("timeline-container").appendTo(this.root);
-        this.timeline = $("<div></div>").addClass("timeline").appendTo(this.container);
-    };
-
-    TimeLine.prototype.Enable = function (callback) {
-        var _this = this;
-        this.CreateDOM();
-
-        var commits = this.serverApi.GetCommitList(this.nodeModel.Case.CaseId);
-        var Case = this.nodeModel.Case;
-        var TopLabel = Case.ElementTop.Label;
-        var decoder = new AssureIt.CaseDecoder();
-        this.timeline.append($('<ul id="timeline-ul"></ul>'));
-        commits.forEach(function (i, v) {
-            $("#timeline-ul").append($('<a id="timeline' + i + '" href="#"></a>').text(v.toString()));
-            $("#timeline" + i).click(function (e) {
-                var loc = _this.serverApi.basepath + "case/" + _this.nodeModel.Case.CaseId;
-                location.href = loc + '/history/' + (i);
-            });
-        });
-    };
-
-    TimeLine.prototype.Disable = function (callback) {
-        $(".timeline-container").remove();
-        callback();
-    };
-    return TimeLine;
-})();
-
 var TimeLinePlugIn = (function (_super) {
     __extends(TimeLinePlugIn, _super);
     function TimeLinePlugIn(plugInManager) {
@@ -72,7 +27,6 @@ var TimeLineMenuPlugIn = (function (_super) {
     TimeLineMenuPlugIn.prototype.Delegate = function (caseViewer, caseModel, element, serverApi) {
         var loc = serverApi.basepath + "case/" + caseModel.Case.CaseId + "/history";
         element.append('<a href="' + loc + '" ><img id="timeline" src="' + serverApi.basepath + 'images/icon.png" title="History" alt="history" /></a>');
-
         return true;
     };
     return TimeLineMenuPlugIn;
--- plugins/TimeLine/TimeLine.ts
+++ plugins/TimeLine/TimeLine.ts
@@ -3,50 +3,6 @@
 /// <reference path="../../src/PlugInManager.ts" />
 /// <reference path="../Editor/Editor.ts" />
 
-class TimeLine {
-	timeline: JQuery;
-	container: JQuery;
-	root: JQuery;
-
-	constructor(public caseViewer: AssureIt.CaseViewer, public nodeModel: AssureIt.NodeModel, public element: JQuery, public serverApi: AssureIt.ServerAPI) {
-	}
-
-	CreateDOM():void {
-		this.root = $(this.caseViewer.Screen.ControlLayer);
-
-		var node: JQuery = $("#"+this.nodeModel.Label);
-
-		this.container = $("<div></div>").css({
-			position: "absolute", left: node.position().left + (node.width() / 2), top: node.position().top + node.height() +  53
-		}).addClass("timeline-container").appendTo(this.root);
-		this.timeline = $("<div></div>")
-			.addClass("timeline")
-			.appendTo(this.container);
-	}
-
-	Enable(callback: () => void):void {
-		this.CreateDOM();
-
-		var commits: AssureIt.CommitCollection = this.serverApi.GetCommitList(this.nodeModel.Case.CaseId);
-		var Case: AssureIt.Case = this.nodeModel.Case;
-		var TopLabel = Case.ElementTop.Label;
-		var decoder   = new AssureIt.CaseDecoder();
-		this.timeline.append($('<ul id="timeline-ul"></ul>'))
-		commits.forEach((i:number, v:AssureIt.CommitModel):void => {
-			$("#timeline-ul").append($('<a id="timeline'+i+'" href="#"></a>').text(v.toString()));
-			$("#timeline"+i).click((e: Event)=>{
-				var loc = this.serverApi.basepath + "case/" + this.nodeModel.Case.CaseId;
-				location.href = loc + '/history/' + (i);
-			});
-		});
-	}
-
-	Disable(callback: ()=>void) {
-		$(".timeline-container").remove();
-		callback();
-	}
-}
-
 class TimeLinePlugIn extends AssureIt.PlugInSet {
 
 	constructor(plugInManager: AssureIt.PlugInManager) {
@@ -70,25 +26,6 @@ class TimeLineMenuPlugIn extends AssureIt.MenuBarContentsPlugIn {
 	Delegate(caseViewer: AssureIt.CaseViewer, caseModel: AssureIt.NodeModel, element: JQuery, serverApi: AssureIt.ServerAPI): boolean {
 		var loc = serverApi.basepath + "case/" + caseModel.Case.CaseId + "/history";
 		element.append('<a href="'+ loc +'" ><img id="timeline" src="' + serverApi.basepath + 'images/icon.png" title="History" alt="history" /></a>');
-
-		//$('#timeline').unbind('click');
-		//$('#timeline').click((ev: Event) => {
-
-		//	var timeline = new TimeLine(caseViewer, caseModel, element, serverApi);
-		//	if(this.visible) {
-		//		timeline.Enable(() => {this.visible = true; });
-		//		this.visible = false;
-		//	}else {
-		//		timeline.Disable(() =>{
-		//			this.visible = true;
-		//		});
-		//	}
-		//	$('#background').click((ev: Event) => {
-		//		timeline.Disable(() =>{
-		//			this.visible = true;
-		//		});
-		//	});
-		//});
 		return true;
 	}
 }
