1b48042c3b92c4d385066aa798afb1671f77f63b
Okamoto Yuki <yuki@Okamotos-MacBook-Pro.local>
2013-09-18 15:08:39 +0900

    move commit button to sidemenu

---
+++ plugins/Commit/Commit.js
@@ -0,0 +1,93 @@
+var __extends = this.__extends || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    __.prototype = b.prototype;
+    d.prototype = new __();
+};
+var CommitWindow = (function () {
+    function CommitWindow() {
+        this.defaultMessage = "Type your commit message...";
+        this.Init();
+    }
+    CommitWindow.prototype.Init = function () {
+        $('#modal-commit').remove();
+        var modal = $('<div id="modal-commit" title="Commit Message" />');
+        (modal).dialog({
+            autoOpen: false,
+            modal: true,
+            resizable: false,
+            draggable: false,
+            show: "clip",
+            hide: "fade"
+        });
+
+        var messageBox = $('<p align="center"></p>');
+        messageBox.append($('<input id="message_box" type="text" size="30" value="' + this.defaultMessage + '" />').css({ 'color': 'gray', 'width': '18em', 'height': '2em' }));
+
+        var commitButton = $('<p align="right"><input id="commit_button" type="button" value="commit"/></p>');
+        modal.append(messageBox);
+        modal.append(commitButton);
+        modal.appendTo($('layer2'));
+    };
+
+    CommitWindow.prototype.SetEventHandlers = function (caseViewer, case0, serverApi) {
+        var self = this;
+
+        $('#message_box').focus(function () {
+            if ($(this).val() == self.defaultMessage) {
+                $(this).val("");
+                $(this).css('color', 'black');
+            }
+        });
+
+        $('#message_box').blur(function () {
+            if ($(this).val() == "") {
+                $(this).val(self.defaultMessage);
+                $(this).css('color', 'gray');
+            }
+        });
+
+        $('#commit_button').click(function () {
+            var encoder = new AssureIt.CaseEncoder();
+            var contents = encoder.ConvertToASN(case0.ElementTop, false);
+
+            if ($("#message_box").val() == self.defaultMessage) {
+                alert("Please put some commit message in the text box.");
+            } else {
+                serverApi.Commit(contents, $("#message_box").val(), case0.CommitId);
+                case0.SetModified(false);
+                window.location.reload();
+            }
+        });
+    };
+    return CommitWindow;
+})();
+
+var CommitPlugIn = (function (_super) {
+    __extends(CommitPlugIn, _super);
+    function CommitPlugIn(plugInManager) {
+        _super.call(this, plugInManager);
+        this.SideMenuPlugIn = new CommitSideMenuPlugIn(plugInManager);
+    }
+    return CommitPlugIn;
+})(AssureIt.PlugInSet);
+
+var CommitSideMenuPlugIn = (function (_super) {
+    __extends(CommitSideMenuPlugIn, _super);
+    function CommitSideMenuPlugIn(plugInManager) {
+        _super.call(this, plugInManager);
+    }
+    CommitSideMenuPlugIn.prototype.IsEnabled = function (caseViewer, Case0, serverApi) {
+        return Case0.IsEditable();
+    };
+
+    CommitSideMenuPlugIn.prototype.AddMenu = function (caseViewer, Case0, serverApi) {
+        var commitWindow = new CommitWindow();
+        commitWindow.SetEventHandlers(caseViewer, Case0, serverApi);
+
+        return new AssureIt.SideMenuModel('#', "Commit", "commit", function (ev) {
+            ($('#modal-commit')).dialog('open');
+        });
+    };
+    return CommitSideMenuPlugIn;
+})(AssureIt.SideMenuPlugIn);
